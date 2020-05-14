5a963fb2ed76a339fafe0ec874794b34f5235413
aryan.programer <aryan.programer@gmail.com>
2020-05-14 14:27:35 +0530

    Add project files.

---
+++ BlockChainBasedInvoiceManagementSystemUi/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace BlockChainBasedInvoiceManagementSystemUi {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+	}
+}
